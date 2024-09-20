02d6327acdb17c6220c53df0a9c6fe2c1f8767d9
asino-nelson <asinonelson276@gmail.com>
2024-09-20 17:16:52 +0300

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -8,23 +8,25 @@ import Cart from "./pages/Cart";
 import Login from "./pages/Login";
 import PlaceOrder from "./pages/PlaceOrder";
 import Orders from "./pages/Orders";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
+      <Navbar />
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/collection" element={<Collection/>}/>
-        <Route path="/about" element={<About/>}/>
-        <Route path="/contact" element={<Contact/>}/>
-        <Route path="/product/:productId" element={<Product/>}/>
-        <Route path="/cart" element={<Cart/>}/>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/place-order" element={<PlaceOrder/>}/>
-        <Route path="/orders" element={<Orders/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/collection" element={<Collection />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/product/:productId" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/place-order" element={<PlaceOrder />} />
+        <Route path="/orders" element={<Orders />} />
       </Routes>
     </div>
-  )
+  );
 }
 
 export default App;
