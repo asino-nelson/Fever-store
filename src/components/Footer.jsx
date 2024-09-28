3acbdd3ea51ef48f64bf67e2902d6389939b77e9
asino-nelson <asinonelson276@gmail.com>
2024-09-28 09:30:49 +0300

    Footer logo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { assets } from "../assets/frontend_assets/assets";
 
 const Footer = () => {
@@ -5,7 +6,9 @@ const Footer = () => {
     <div>
       <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
         <div>
-          <img src={assets.logo} className="mb-5 w-32" alt="logo" />
+          <Link to="/">
+            <h1 className="w-36 mb-5 font-bold text-2xl">FeverGifts</h1>
+          </Link>
           <p className="w-full md:w-2/3 text-gray-600">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit.
           </p>
@@ -31,7 +34,9 @@ const Footer = () => {
       </div>
       <div>
         <hr />
-        <p className="py-5 text-sm text-center">© 2024 fevergifts.com - All Rights Reserved</p>
+        <p className="py-5 text-sm text-center">
+          © 2024 fevergifts.com - All Rights Reserved
+        </p>
       </div>
     </div>
   );
