9ab50a3662c9e021d3a1cd59a1436bde75c66031
asino-nelson <asinonelson276@gmail.com>
2024-10-04 19:04:00 +0300

    cart functionality

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,20 +1,19 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useState } from "react";
 import { products } from "../assets/frontend_assets/assets";
 
 export const ShopContext = createContext();
 
 const ShopContextProvider = (props) => {
-  const currency = "Ksh";
+  const currency = "Ksh.";
   const delivery_fee = 10;
   const [search, setSearch] = useState("");
   const [showSearch, setShowSearch] = useState(false);
   const [cartItems, setCartItems] = useState({});
 
   const addToCart = async (itemId, size) => {
-
     if (!size) {
-      alert("Select Product Size")
-      return
+      alert("Select Product Size");
+      return;
     }
 
     let cartData = structuredClone(cartItems);
@@ -30,14 +29,30 @@ const ShopContextProvider = (props) => {
       cartData[itemId][size] = 1;
     }
 
-    setCartItems(cartData)
+    setCartItems(cartData);
   };
 
-  useEffect(() => {
-    console.log(cartItems);
-    
-  }, [cartItems])
-  
+  const getCartCount = () => {
+    let totalCount = 0;
+    for (const items in cartItems) {
+      for (const item in cartItems[items]) {
+        try {
+          if (cartItems[items][item] > 0) {
+            totalCount += cartItems[items][item];
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    }
+    return totalCount;
+  };
+
+  const updateQuantity = async (itemId, size, quantity) => {
+    let cartData = structuredClone(cartItems);
+    cartData[itemId][size] = quantity;
+    setCartItems(cartData);
+  };
 
   const value = {
     products,
@@ -47,7 +62,10 @@ const ShopContextProvider = (props) => {
     setSearch,
     showSearch,
     setShowSearch,
-    cartItems, addToCart
+    cartItems,
+    addToCart,
+    getCartCount,
+    updateQuantity,
   };
   return (
     <ShopContext.Provider value={value}>{props.children}</ShopContext.Provider>
