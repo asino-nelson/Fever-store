2a06fee346ab6b287f492e41706fbeeb318398bc
asino-nelson <asinonelson276@gmail.com>
2024-10-08 01:11:52 +0300

    Contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,43 @@
+import { assets } from "../assets/frontend_assets/assets";
+import NewsLetter from "../components/NewsLetter";
+import Title from "../components/Title";
 
 const Contact = () => {
   return (
     <div>
-      
-    </div>
-  )
-}
+      <div className="text-2xl text-center pt-10 border-t">
+        <Title text1={"CONTACT"} text2={"US"} />
+      </div>
+      <div className="my-10 flex flex-col justify-center md:flex-row gap-10 mb-28">
+        <img
+          className="w-full md:max-w-[480px]"
+          src={assets.contact_img}
+          alt=""
+        />
+        <div className="flex flex-col justify-center items-start gap-6">
+          <p className="font-semibold text-xl text-gray-600">Our Store</p>
+          <p className="text-gray-500">
+            54709 Prime Plaza <br /> Kimathi 350, Nairobi
+          </p>
+          <p className="text-gray-500">
+            Tel: +254 7** *** *** <br /> Email: fevergifts@gmail.com
+          </p>
 
-export default Contact
+          <p className="font-semibold text-xl text-gray-600">
+            Careers at Forever
+          </p>
+          <p className="text-gray-500">
+            Learn more about our teams and job openings.
+          </p>
+
+          <button className="border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500">
+            Explore Jobs
+          </button>
+        </div>
+      </div>
+      <NewsLetter />
+    </div>
+  );
+};
+
+export default Contact;
